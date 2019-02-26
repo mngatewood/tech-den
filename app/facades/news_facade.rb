4fbddf67967fde03bb9bf57a151cb5e0fbd1b9c9
Michael Gatewood <michael@mngatewood.com>
2019-02-26 14:21:26 -0700

    Add "Added to Favorites" to article * add is_favorite? method to news facade * add flash message * style flash message

--- app/facades/news_facade.rb
+++ app/facades/news_facade.rb
@@ -1,5 +1,9 @@
 class NewsFacade
 
+  def is_favorite?(url)
+    Favorite.where({url: url}).exists?
+  end
+
   def articles
     response = Faraday.get("https://newsapi.org/v2/everything?q=%22denver%20technology%22%20OR%20%22denver%20tech%22&sortBy=publishedAt&apiKey=#{ENV['NEWS_API_KEY']}")
     body = JSON.parse(response.body, :symbolize_names => true)
