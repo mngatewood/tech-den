6109d25ede3cf8b2c7780b1f8278bceee2c7b663
Michael Gatewood <michael@mngatewood.com>
2019-02-26 19:26:31 -0700

    Convert published date to hours/days/weeks ago * add method to convert datetime to time ago * add method to add age to article * update html to use age instead of published at * add minor CSS

--- app/facades/news_facade.rb
+++ app/facades/news_facade.rb
@@ -13,7 +13,35 @@ class NewsFacade
     unique_url = articles.uniq {|article| article[:url]}
     # remove duplicate titles
     unique_title = unique_url.uniq {|article| article[:title]}
-    return unique_title
+    # add age of article
+    article_age = unique_title.map do |article| 
+      article.update({age: convertDateToAge(article[:publishedAt])})
+    end
+    return article_age
+  end
+
+  def convertDateToAge(date)
+    current_time = Time.now.utc.to_s;
+    article_time = DateTime.strptime(date).to_s;
+    seconds = Time.parse(current_time) - Time.parse(article_time);
+    hours = (seconds / 3600).round
+    days = (hours / 24).round
+    weeks = (days / 7).round
+    if weeks > 1
+      return weeks.to_s + " weeks ago"
+    elsif weeks == 1
+      return weeks.to_s + " week ago"
+    elsif days > 1
+      return days.to_s + " days ago"
+    elsif days == 1
+      return days.to_s + " day ago"
+    elsif hours > 1
+      return hours.to_s + " hours ago"
+    elsif hours == 1
+      return hours.to_s + " hour ago"
+    else
+      return "Less than an hour ago."
+    end
   end
 
   def news
